a1aad203d6809c5035c224a4954acefafa210b1f
SecretDzen <turretbox@gmail.com>
2021-10-19 13:06:21 +0700

    feat(mk-game): Added new logic of game. New Styles

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const $arena = document.querySelector('.arenas');
+const $randButton = document.querySelector('.button');
 
 const player1 = {
   name: 'KITANA',
@@ -6,9 +7,18 @@ const player1 = {
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
   weapon: ['Glaive'],
-  attack: function () {
-    console.log(this.name + ' Fight...');
+  changeHP: function (num) {
+    this.hp -= num;
+    if (this.hp <= 0) {
+      this.hp = 0;
+    }
   },
+  elHP: function () {
+    return document.querySelector(`.player${this.player} .life`);
+  },
+  renderHP: function ($elem) {
+    $elem.style.width = `${this.hp}%`;
+  }
 };
 
 const player2 = {
@@ -17,9 +27,18 @@ const player2 = {
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
   weapon: ['Longsword'],
-  attack: function () {
-    console.log(this.name + ' Fight...');
+  changeHP: function (num) {
+    this.hp -= num;
+    if (this.hp <= 0) {
+      this.hp = 0;
+    }
   },
+  elHP: function () {
+    return document.querySelector(`.player${this.player} .life`);
+  },
+  renderHP: function ($elem) {
+    $elem.style.width = `${this.hp}%`;
+  }
 };
 
 const createElem = (tag, className) => {
@@ -56,42 +75,46 @@ const createPlayer = (object) => {
   return $player;
 };
 
-const $randButton = document.querySelector('.button');
 $randButton.addEventListener('click', () => {
-  changeHP(player1);
-  changeHP(player2);
-  if (player1.hp <= 0 && player2.hp > 0) {
-    $arena.appendChild(playerWin(player2.name));
-  }
-  if (player2.hp <= 0 && player1.hp > 0) {
-    $arena.appendChild(playerWin(player1.name));
-  }
-  if (player2.hp <= 0 && player1.hp <= 0) {
-    $arena.appendChild(playersDraw());
+  player1.changeHP(getRandom(20));
+  player1.renderHP(player1.elHP());
+  player2.changeHP(getRandom(20));
+  player2.renderHP(player2.elHP());
+  if (player1.hp === 0 || player2.hp === 0) {
+    $randButton.style = 'display: none';
+    const title = 
+    player1.hp === 0
+    ? player2.hp === 0
+    ? playerWin()
+    : playerWin(player2.name)
+    : playerWin(player1.name);
+    $arena.appendChild(title);
+    $arena.appendChild(createReloadButton());
   }
 });
 
-const changeHP = (object) => {
-  const $playerLife = document.querySelector(`.player${object.player} .life`);
-  object.hp -= Math.ceil(Math.random() * 20);
-  if (object.hp <= 0) {
-    $playerLife.style.width = 0;
-    $randButton.style = 'display: none';
-  } else {
-    $playerLife.style.width = `${object.hp}%`;
-  }
+function getRandom(num) {
+  return Math.ceil(Math.random() * num);
 }
 
 const playerWin = (name) => {
   const $winTitle = createElem('div', 'loseTitle');
-  $winTitle.innerText = `${name} wins`;
+  if (name) {
+    $winTitle.innerText = `${name} wins`;
+  } else {
+    $winTitle.innerText = 'Draw';
+  }
+
   return $winTitle;
 }
 
-const playersDraw = () => {
-  const $drawTitle = createElem('div', 'loseTitle');
-  $drawTitle.innerText = 'Draw';
-  return $drawTitle;
+function createReloadButton() {
+  const $reloadWrap = createElem('div', 'reloadWrap');
+  const $reloadButton = createElem('button', 'button');
+  $reloadButton.innerText = 'Reload';
+  $reloadButton.addEventListener('click', () => window.location.reload());
+  $reloadWrap.appendChild($reloadButton);
+  return $reloadWrap;
 }
 
 $arena.appendChild(createPlayer(player1));
